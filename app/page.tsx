9bf7e52f75ac29141fe9356ce9f5aecd35430c76
benjamin10ks <bjkmanning@gmail.com>
2025-02-11 12:50:26 -0700

    deez

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { useRef } from "react";
-import ImageUploader from "@/components/ImageUploader"; // Adjust path if necessary
+import ImageUploader from "@/components/ImageUploader";
 import Draggable from "react-draggable";
 
 export default function Home() {
-  const nodeRef = useRef(null); // Create a ref for Draggable
+  const nodeRef = useRef<HTMLDivElement | null>(null); // Fix: Use HTMLDivElement
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Upload an Image</h1>
       <Draggable nodeRef={nodeRef}>
-        <div ref={nodeRef}>
+        <div className="hover: cursor-pointer" ref={nodeRef}>
           <ImageUploader />
         </div>
       </Draggable>
